cb5bbb6a705006aacc186246c9e997755b3ede53
Pavan-Soma-Shekar <114622393+Pavan-Soma-Shekar@users.noreply.github.com>
2023-10-12 11:56:57 -0400

    Bug fixes2 (#16)
    * Updated the database connection in the yml file
    
    * Updated the yml file to fix bugs
    
    * Bug-fix updated the yml file
    
    * Bug-Fixes yml file
    
    * Bug-Fixes2 updated the yml file

--- AssignmentsAPI/src/utilities/connection.js
+++ AssignmentsAPI/src/utilities/connection.js
@@ -10,11 +10,11 @@ dotenv.config();
 
 const sequelize = new Sequelize({
     dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'pavan',
-    database: 'AssignmentsDb',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 const User = sequelize.define('User', {
