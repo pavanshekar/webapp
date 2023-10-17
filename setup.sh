e05c0c6e8e29fa664366512ce62c2ef3dd1ae0d4
Pavan-Soma-Shekar <shekar.p@northeastern.edu>
2023-10-17 16:13:31 -0400

    Updated the shell script to resolve the mariadb errors

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ -f .env ]; then
   export $(cat .env | xargs)
@@ -6,23 +7,21 @@ fi
 
 sudo apt update 
 sudo apt upgrade -y
-
 sudo apt install -y unzip mariadb-server
 
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY ${DB_PASSWORD};"
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
-sudo mysql -e "GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO ${DB_USER}@'localhost' IDENTIFIED BY ${DB_PASSWORD};"
-sudo mysql -e "FLUSH PRIVILEGES;"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY '${DB_PASSWORD}';" | sudo mariadb
+echo "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};" | sudo mariadb
+echo "GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';" | sudo mariadb
+echo "FLUSH PRIVILEGES;" | sudo mariadb
 
 sudo mkdir -p /opt/AssignmentsAPI
 sudo unzip /tmp/app.zip -d /opt/AssignmentsAPI
 cd /opt/AssignmentsAPI/AssignmentsAPI
 
 sudo npm install
-
 node app.js
 
 sudo apt-get clean
