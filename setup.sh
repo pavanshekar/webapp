d88514794a0bd3159c59575b86e7f608ac567313
Pavan-Soma-Shekar <shekar.p@northeastern.edu>
2023-10-17 14:59:32 -0400

    Updated setup.sh and packer build workflow

--- setup.sh
+++ setup.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
-# Load environment variables from the .env file
 if [ -f .env ]; then
   export $(cat .env | xargs)
 fi
 
-# Update packages
 sudo apt update 
 sudo apt upgrade -y
-
-# Install unzip and other necessary packages
 sudo apt install -y unzip mariadb-server
 
-# Set up NodeJS using the nodesource repository
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY '${DB_PASSWORD}';"
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS '${DB_DATABASE}';"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
 sudo mysql -e "GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
 sudo mysql -e "FLUSH PRIVILEGES;"
 
-# Extract and set up the application
 sudo mkdir -p /opt/AssignmentsAPI
 sudo unzip /tmp/app.zip -d /opt/AssignmentsAPI
 cd /opt/AssignmentsAPI/AssignmentsAPI
 sudo npm install
 
-# Clean up
+node app.js
+
 sudo apt-get clean
