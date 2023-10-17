d1945ee1bfe4ffdf92281464160f517b11ec457a
Pavan-Soma-Shekar <shekar.p@northeastern.edu>
2023-10-17 15:56:01 -0400

    Updated the shell script

--- setup.sh
+++ setup.sh
@@ -6,19 +6,21 @@ fi
 
 sudo apt update 
 sudo apt upgrade -y
+
 sudo apt install -y unzip mariadb-server
 
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY '${DB_PASSWORD}';"
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY ${DB_PASSWORD};"
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
-sudo mysql -e "GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO ${DB_USER}@'localhost' IDENTIFIED BY ${DB_PASSWORD};"
 sudo mysql -e "FLUSH PRIVILEGES;"
 
 sudo mkdir -p /opt/AssignmentsAPI
 sudo unzip /tmp/app.zip -d /opt/AssignmentsAPI
 cd /opt/AssignmentsAPI/AssignmentsAPI
+
 sudo npm install
 
 node app.js
