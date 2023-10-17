7cd7602cda55ea1dedf69d32d9ba6277f433ac9a
Pavan-Soma-Shekar <shekar.p@northeastern.edu>
2023-10-17 17:30:43 -0400

    Updated shell script

--- setup.sh
+++ setup.sh
@@ -7,21 +7,17 @@ fi
 
 sudo apt update 
 sudo apt upgrade -y
-sudo apt install -y unzip mariadb-server
 
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-sudo systemctl start mariadb
+sudo apt install -y mariadb-server
 
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${DB_PASSWORD}');" | sudo mariadb
-echo "FLUSH PRIVILEGES;" | sudo mariadb
+printf "%s\nno\nno\nyes\nyes\nyes\nyes\n" "$DB_PASSWORD" | sudo mysql_secure_installation -p$DB_PASSWORD
 
-echo "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};" | sudo mariadb
-echo "FLUSH PRIVILEGES;" | sudo mariadb
+sudo mariadb -u root -p$DB_PASSWORD -e "CREATE DATABASE $DB_DATABASE;"
 
-echo "GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO 'root'@'localhost';" | sudo mariadb
-echo "FLUSH PRIVILEGES;" | sudo mariadb
+sudo apt install -y unzip
 
 sudo mkdir -p /opt/AssignmentsAPI
 sudo unzip /tmp/app.zip -d /opt/AssignmentsAPI
