14351bfd3a95049ab2af933570a2fe24b733cfd4
Pavan-Soma-Shekar <shekar.p@northeastern.edu>
2023-11-07 07:10:27 -0500

    Updated the logger file to include the file logs

--- AssignmentsAPI/src/utilities/logger.js
+++ AssignmentsAPI/src/utilities/logger.js
@@ -1,35 +1,63 @@
 const winston = require('winston');
 const winstonCloudWatch = require('winston-cloudwatch');
- 
+const fs = require('fs');
+const path = require('path');
+
+const logDir = '/var/log'; 
+const logFilePath = path.join(logDir, 'csye6225.log'); 
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const fileFormat = winston.format.combine(
+  winston.format.timestamp({
+    format: 'YYYY-MM-DDTHH:mm:ssZ'
+  }),
+  winston.format.printf(info => {
+    return `${info.timestamp} ${info.level}: ${info.message}`;
+  })
+);
+
 var options = {
   console: {
     level: 'debug',
     handleExceptions: true,
     json: false,
     colorize: true,
-    timestamp: true,
+  },
+  file: {
+    level: 'debug',
+    filename: logFilePath, 
+    handleExceptions: true,
+    json: false,
+    maxsize: 5242880,
+    maxFiles: 5,
+    colorize: false,
+    format: fileFormat, 
   },
 };
- 
+
 const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(options.console),
+    new winston.transports.File(options.file),
     new winstonCloudWatch({
       logGroupName: 'csye6225',
       logStreamName: 'webapp',
       awsRegion: 'us-east-2',
-      retentionInDays: 1,
-    }),
+      jsonMessage: false, 
+      retentionInDays: 1
+    })
   ],
-  exitOnError: false,
+  exitOnError: false, 
 });
- 
+
 logger.level = 'silly';
- 
+
 logger.stream = {
-  write: function (message, encoding) {
-    logger.info(message);
+  write: function(message, encoding) {
+    logger.info(message.trim());
   },
 };
- 
+
 module.exports = logger;
